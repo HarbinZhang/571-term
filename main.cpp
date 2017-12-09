088aee00198de45db1829c9ca04f07c9a091135e
HarbinZhang <seashore.real@gmail.com>
2017-12-08 21:34:30 -0500

    hi

--- main.cpp
+++ main.cpp
@@ -5,19 +5,20 @@ using namespace std;
 
 // Task(TaskType type, int release_time, int deadline, int exe_time, int period)
 int main(){
-	EDF* edf = new EDF();
-	edf->push(new Task(1, 0, 3, 1, 10));
-	edf->push(new Task(2, 0, 4, 1, 10));
-	edf->push(new Task(3, 0, 5, 1, 10));
-	edf->push(new Task(4, 0, 6, 1, 10));
-	edf->push(new Task(5, 0, 7, 1, 10));
-	edf->push(new Task(6, 0, 8, 1, 10));
-	edf->push(new Task(7, 0, 9, 1, 10));
-	while(!edf->empty()){
-		usleep(200000);
-		edf->increaseTime_1();
-	}
+	// EDF* edf = new EDF();
+	// edf->push(new Task(1, 0, 3, 2, 10));
+	// edf->push(new Task(2, 0, 2, 1, 10));
+	// edf->push(new Task(3, 0, 5, 1, 10));
+	// edf->push(new Task(4, 0, 6, 1, 10));
+	// edf->push(new Task(5, 0, 7, 1, 10));
+	// edf->push(new Task(6, 0, 8, 1, 10));
+	// edf->push(new Task(7, 0, 9, 1, 10));
+	// while(!edf->empty()){
+	// 	usleep(200000);
+	// 	edf->increaseTime_1();
+	// }
 
+// Task(TaskType type, int release_time, int deadline, int exe_time, int period)
 	// LST* lst = new LST();
 	// lst->push(new Task(PeriodicTask, 0, 3, 1, 10));
 	// lst->push(new Task(PeriodicTask, 0, 4, 1, 10));
@@ -32,18 +33,17 @@ int main(){
 	// }
 
 
-	// RMS* rms = new RMS();
-	// rms->push(new Task(1, 0, 10, 10, 10));
-	// rms->push(new Task(2, 0, 10, 3, 0));
-	// // rms->push(new Task(3, 5, 6, 2, 0));
-	// // rms->push(new Task(5, 2, 2, 4, 4));
+// Task(TaskType type, int release_time, int deadline, int exe_time, int period)
+	RMS* rms = new RMS();
+	// rms->push(new Task(1, 0, 10, 5, 10));
+	rms->push(new Task(2, 0, 10, 3, 0));
+	rms->push(new Task(3, 5, 6, 2, 0));
+	rms->push(new Task(4, 0, 2, 4, 4));
 
-	// while(!rms->empty()){
-	// 	usleep(200000);
-	// 	rms->increaseTime_1();
-	// 	// rms->getU();
-	// }
-
-
+	while(!rms->empty()){
+		usleep(400000);
+		rms->increaseTime_1();
+		// rms->getU();
+	}
 
 }
